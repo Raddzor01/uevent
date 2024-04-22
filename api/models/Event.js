5da217937f511a45ca07e6e9e06ab16a9d77c498
raddz <danil.garin.04@gmail.com>
2024-04-22 21:24:43 +0300

    get company events request fix

--- api/models/Event.js
+++ api/models/Event.js
@@ -13,35 +13,16 @@ export default class Event extends Model {
         return res[0].insertId;
     }
 
-    async saveCalendarEvent(eventId, calendarId, role = "admin") {
-        const query = `INSERT INTO calendarevents(eventId, calendarId, role) VALUES(?, ?, ?);`;
-        const res = await dbService.makeRequest(query, [eventId, calendarId, role]);
-
-        return res[0].insertId;
-    }
-
     async read(id) {
         const data = await super.read(id);
         return data[0][0];
     }
 
-    async checkPermission(calendarId, eventId) {
-        const query = `SELECT role FROM calendarevents WHERE calendarId = ? AND eventId = ?; `;
-        const rows = await dbService.makeRequest(query, [calendarId, eventId]);
-
-        return rows.length > 0 && rows[0].role === "admin";
-    }
 
     async getAllCompanyEvents(companyId) {
-        const query = `SELECT * FROM events 
-                            INNER JOIN companies ON events.company_id = companies.id
-                            WHERE company_id = ?`;
+        const query = `SELECT * FROM events WHERE company_id = ?`;
         const rows = await dbService.makeRequest(query, [companyId]);
         return rows[0];
     }
 
-    async removeEventFromCalendar(calendarId, eventId) {
-        const query = `DELETE FROM calendarevents WHERE calendarId = ? AND eventId = ?; `;
-        await dbService.makeRequest(query, [calendarId, eventId]);
-    }
 }
