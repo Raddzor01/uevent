8bab39edc57bc5037b14fa6316287dae4a61d261
raddz <danil.garin.04@gmail.com>
2024-04-23 20:39:17 +0300

    replace bcrypt to argon2

--- api/utils/hashService.js
+++ api/utils/hashService.js
@@ -1,12 +1,12 @@
 import bcrypt from 'bcrypt';
-
+import argon2 from 'argon2';
 const hashPassword = async(password) => {
     const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
+    return await argon2.hash(password, salt);
 };
 
 const comparePasswords = async(password, hash) => {
-    return bcrypt.compare(password, hash);
+    return await argon2.verify(hash, password);
 };
 
 
